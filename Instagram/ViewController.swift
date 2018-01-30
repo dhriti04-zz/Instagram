ad99ba42811f3d24b750f140ee899160ce3152be
Dhriti <dhritichawla@pal-nat186-88-198.itap.purdue.edu>
2018-01-29 19:27:59 -0500

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Dhriti Chawla on 1/29/18.
+//  Copyright © 2018 Dhriti Chawla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
