32e6a521cc14efe67eba90aaf7c088fccda2a440
Dhriti Chawla <dhritichawla@pal-nat186-94-3.itap.purdue.edu>
2018-02-04 15:31:03 -0500

    First commit

---
+++ Instagram/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  Instagram
+//
+//  Created by Dhriti Chawla on 2/3/18.
+//  Copyright © 2018 Dhriti Chawla. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Instagram/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  Instagram
+//
+//  Created by Dhriti Chawla on 2/3/18.
+//  Copyright © 2018 Dhriti Chawla. All rights reserved.
+//
+
+import UIKit
+
+class Post: PFObject {
+
+}
